5393f0c65fb75e53e90f64ba2f2781b1bdb81235
Raman Hrytsuk <Raman_Hrytsuk@epam.com>
2019-06-11 11:48:19 +0300

    12

---
+++ TorrentsWebApp/Models/File.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace TorrentsWebApp.Models
+{
+    public  class File
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Size { get; set; }
+        public int TorrentId { get; set; }
+
+    }
+}
---
+++ TorrentsWebApp/Models/Forum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TorrentsWebApp.Models
+{
+    public partial class Forum
+    {
+       
+
+        public int Id { get; set; }
+        public string Value { get; set; }
+
+       
+    }
+}
